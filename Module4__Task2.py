89ff7b5d5631d60c58c7af4590d83de358ab8010
Aleksandra-Vainilovich <57612894+Aleksandra-Vainilovich@users.noreply.github.com>
2021-09-28 17:53:45 +0300

    Commit 2

--- Module4__Task2.py
+++ Module4__Task2.py
@@ -1,11 +1,9 @@
 import random
 import string
 
-# Return and print 100 randomly selected integer values from 1 to 1000
+# Return and print c randomly selected integer values from a to b
 def random_number_list(a, b, c):
     return [random.randrange(a, b) for i in range(c)]
-
-random_number_list = random_number_list(1, 26, 26)
 
 
 def random_dictionary(a):
@@ -13,19 +11,14 @@ def random_dictionary(a):
     for z in random_number_list:
         random_dictionary = [{str(j): random.randrange(1, 100) for z in range(1) for j in random.choices(string.ascii_lowercase, k= random_number_list[a])}
           , {str(j): random.randrange(1, 100) for z in range(1) for j in random.choices(string.ascii_lowercase, k= random_number_list[a])}]
-    return random_dictionary
-
-
-random_dictionary = random_dictionary(3)
-random_dict = random_dictionary.copy()
-print(f'Random dictionary:\n{random_dict}')
+        return random_dictionary
 
 
 def updated_dictionary():
     updated_dict = {}
-    for dictionary in random_dict:
-        updated_dict = random_dict[1]
-        for key, value in random_dict[0].items():
+    for dictionary in random_dictionary:
+        updated_dict = random_dictionary[1]
+        for key, value in random_dictionary[0].items():
             if key in updated_dict:
                 if updated_dict[key] > value:
                     key_updated, updated_dict[key_updated] = f'{key}_2', updated_dict[key]
@@ -37,8 +30,17 @@ def updated_dictionary():
                 updated_dict[key] = value
             if key in updated_dict:
                 updated_dict[key] = updated_dict.pop(key)
-        return updated_dict
+        return f'Updated dictionary:\n{updated_dict}'
 
 
-print(f'upd:\n{updated_dictionary()}')
+# Enter c integer values from a to b: (a, b, c)
+random_number_list = random_number_list(1, 26, 26)
+print(random_number_list)
+
+# enter random number to set dictionary length
+random_dictionary(3)
+print(f'Random dictionary:\n{random_dictionary}')
+
+print(updated_dictionary())
+
 
