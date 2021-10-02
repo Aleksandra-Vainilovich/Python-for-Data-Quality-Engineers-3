addb30b3828688bcd5c9ed71e0fc5e43d352838e
Aleksandra-Vainilovich <57612894+Aleksandra-Vainilovich@users.noreply.github.com>
2021-10-02 15:11:09 +0300

    Commit 5

--- Module5.0.py
+++ Module5.0.py
@@ -6,7 +6,7 @@ class PrintMessage:
         self.message = message
 
     def print_message(self):
-        ptf = open("Info.txt", "a")
+        ptf = open("Module5.0.txt", "a")
         print(self.message, file=ptf)
         ptf.close()
 
@@ -17,19 +17,18 @@ class News:
         self.location = location
 
     def news_message(self):
-        message = f'News -------------------------\n{self.news_msg}\n{self.location}, {datetime.datetime.now()}\n\n'
+        message = f'News -------------------------\n{self.news_msg}\n{self.location}, {datetime.datetime.now().strftime("%d/%m/%y %H:%M")}\n\n'
         self.prt = PrintMessage(message)
         prt = self.prt
         prt.print_message()
 
 class Advertising:
-    def __init__(self, adv_message, location, actual_until=None):
+    def __init__(self, adv_message, actual_until=None):
         self.adv_message = adv_message
-        self.location = location
         self.actual_until = actual_until
 
     def advertising(self):
-        message = f'News -------------------------\n{self.adv_message}\n{self.location}, {self.actual_until} , {(datetime.datetime.strptime(self.actual_until, "%m/%d/%y") - datetime.datetime.now()).days}\n\n'
+        message = f'Private Ad ------------------\n{self.adv_message}\nActual until: {self.actual_until}, {(datetime.datetime.strptime(self.actual_until, "%d/%m/%y") - datetime.datetime.now()).days} days left\n\n'
         self.prt = PrintMessage(message)
         prt = self.prt
         prt.print_message()
@@ -41,7 +40,7 @@ class WhoIs:
 
     def ask_question(self):
         rand_num_list = [random.randrange(1, 100) for i in range(1)]
-        question = f'Who killed Kennedy?\n{self.answer}\nprobability: {rand_num_list[0]} %\n\n'
+        question = f'How do you think? ----------\nWho killed Kennedy?\n{self.answer}\nprobability: {rand_num_list[0]} %\n\n'
         self.prt = PrintMessage(question)
         prt = self.prt
         prt.print_message()
@@ -57,17 +56,16 @@ class Choice:
             news_mess = self.news
             news_mess.news_message()
         elif self.flag == '2':
-            self.advng = Advertising(input('Please enter advertisment text\n'), input('Please enter location\n'),
-                                     input('Please enter due date in the format mm/dd/yy\n'))
+            self.advng = Advertising(input('Please enter advertisment text\n'), input('Please enter expire date in the format dd/mm/yy\n'))
             adv_message = self.advng
             adv_message.advertising()
         elif self.flag == '3':
-            self.question = WhoIs(input('Please enter your assumption\n'))
+            self.question = WhoIs(input('Who killed Kennedy? Please enter your assumption\n'))
             question_mess = self.question
             question_mess.ask_question()
         else:
             print('Your choice is not correct')
 
 
-make_your_choice = Choice(input('Please enter your choice\n'))
+make_your_choice = Choice(input('Please enter your choice:\n1 for News message\n2 for Advertisment\n3 for question of a day\n'))
 make_your_choice.choose_message_type()
