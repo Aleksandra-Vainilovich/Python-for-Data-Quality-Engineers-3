458efcdb5d983cf4405475cccdaed377f07e8a6f
Aleksandra-Vainilovich <57612894+Aleksandra-Vainilovich@users.noreply.github.com>
2021-10-19 15:54:24 +0300

    Commit 1

---
+++ Module8/Module4_task3.py
@@ -0,0 +1,65 @@
+# import module re
+import re
+
+
+# Last words of each existing sentence
+def findall_last_word(a, b):
+    return re.findall(a, b)
+
+
+# One more sentence with last words of each existing sentence
+def new_sentence():
+    variable = findall_last_words[0]
+    view = findall_last_words[1]
+    paragraph = findall_last_words[2]
+    here = findall_last_words[3]
+    mistake = findall_last_words[4]
+    text = findall_last_words[5]
+    whitespaces = findall_last_words[6]
+    nbr = findall_last_words[7]
+    return f'\n\n\t{here} is a {mistake} in {text} {paragraph}. To my {view} {variable} should have more than {nbr} {whitespaces}.\n'
+
+
+def splitted(a):
+    # Add new sentence to the end of this paragraph
+    message = initial_message + new_sentence()
+    # split message on pattern
+    splitteds = re.split(a, message)
+    # capitalize first letter or sentences
+    capital_letters = [i.capitalize() for i in splitteds]
+    # join all parts back
+    return ''.join(capital_letters)
+
+
+# Replace iz by is if necessary
+def misspelling(a, b):
+    return splitteds.replace(a, b)
+
+
+# Calculate number of whitespace characters in this text
+def filtered_whitesp():
+    return len(list(s for s in misspelling if s.isspace()))
+
+
+# Text variable
+initial_message = """tHis iz your homeWork, copy these Text to variable. 
+
+	You NEED TO normalize it fROM letter CASEs point oF View. also, create one MORE senTENCE witH LAST WoRDS of each existING SENtence and add it to the END OF this Paragraph.
+
+	it iZ misspeLLing here. fix“iZ” with correct “is”, but ONLY when it Iz a mistAKE. 
+
+	last iz TO calculate nuMber OF Whitespace characteRS in this Text. caREFULL, not only Spaces, but ALL whitespaces. I got 87."""
+
+
+findall_last_words = findall_last_word('\s(\w+)?\.', initial_message)
+
+splitteds = splitted('(\.\s+)')
+
+misspelling = misspelling(" iz", " is")
+print(f'Corrected mistakes:\n{misspelling}')
+
+
+print(f'Number of whitespaces: {filtered_whitesp()}')
+
+
+
---
+++ Module8/Module8.py
@@ -0,0 +1,166 @@
+import os
+import re
+
+import datetime
+import random
+
+import Module4_task3
+
+import json
+
+
+class PrintMessage:
+    def __init__(self, message):
+        self.message = message
+
+    def print_message(self):
+        ptf = open("Module6.0.txt", "a")
+        print(self.message, file=ptf)
+        ptf.close()
+
+
+class News:
+    def __init__(self, news_msg, location):
+        self.news_msg = news_msg
+        self.location = location
+
+    def news_message(self):
+        message = f'News -------------------------\n{self.news_msg}\n{self.location}, {datetime.datetime.now().strftime("%d/%m/%y %H:%M")}\n\n'
+        self.prt = PrintMessage(message)
+        prt = self.prt
+        prt.print_message()
+
+
+class Advertising:
+    def __init__(self, adv_message, actual_until=None):
+        self.adv_message = adv_message
+        self.actual_until = actual_until
+
+    def advertising(self):
+        message = f'Private Ad ------------------\n{self.adv_message}\nActual until: {self.actual_until}, {(datetime.datetime.strptime(self.actual_until, "%d/%m/%y") - datetime.datetime.now()).days} days left\n\n'
+        self.prt = PrintMessage(message)
+        prt = self.prt
+        prt.print_message()
+
+
+class WhoIs:
+    def __init__(self, answer):
+        self.answer = answer
+
+    def ask_question(self):
+        rand_num_list = [random.randrange(1, 100) for i in range(1)]
+        question = f'How do you think? ----------\nWho killed Kennedy?\n{self.answer}\nprobability: {rand_num_list[0]} %\n\n'
+        self.prt = PrintMessage(question)
+        prt = self.prt
+        prt.print_message()
+
+
+class AddInfo:
+    def __init__(self, source, target):
+        self.source = source
+        self.target = target
+
+    def add_message(self):
+        # Open source file to read
+        open_source = open(self.source, 'r')
+        # Open target file to appending to the end of the file
+        open_target = open(self.target, 'a')
+        # Write message from source file into target file
+        for txt in open_source:
+            open_target.write(txt)
+        open_target.close()
+        open_target = open(self.target, 'r')
+        # If message was successfully written into target file, remove source file
+        if txt in open_target:
+            open_source.close()
+            os.remove(self.source)
+        else:
+            open_source.close()
+            print('File was not successfully processed')
+
+    def normalize(self):
+        # Replace target words by spaces
+        Module4_task3.findall_last_words = Module4_task3.findall_last_word(' ', '                ')
+        # Send extra sentences, that are not ot be printed in target file, into variable
+        remove_message = Module4_task3.new_sentence()
+
+        # Open target file to read
+        open_file = open(self.target, 'r')
+        # Copy text from target file
+        Module4_task3.initial_message = open_file.read()
+        open_file.close()
+
+        # Open target file for writing, truncating the file first
+        open_file = open(self.target, 'w')
+        # Apply normalization from Module4_task3.py with corresponding trigger
+        normalized_text = Module4_task3.splitted('(\.\s+|\n)')
+        # Remove extra sentences, that are not ot be printed in target file
+        normalized_text = normalized_text.replace(remove_message, '', re.I)
+        # Write final text into target file
+        open_file.write(normalized_text)
+        open_file.close()
+
+
+class AddJson:
+    def __init__(self, source):
+        self.source = source
+
+    def parse_json(self):
+        try:
+
+            j_son = json.load(open(self.source))
+            for i in j_son:
+                if i['type'] == 'News -------------------------':
+                    self.news = News(i['body'], i['location'])
+                    news_mess = self.news
+                    news_mess.news_message()
+
+                elif i['type'] == 'Private ad ------------------':
+                    self.advng = Advertising(i['body'], i['date'])
+                    adv_message = self.advng
+                    adv_message.advertising()
+
+                elif i['type'] == 'How do you think? ----------':
+                    self.question = WhoIs(i['location'])
+                    question_mess = self.question
+                    question_mess.ask_question()
+
+        except:
+            print('!!!Incorrect JSON format. Import failed!!!')
+
+
+class Choice:
+    def __init__(self, flag):
+        self.flag = flag
+
+    def choose_message_type(self):
+        if self.flag == '1':
+            self.news = News(input('Please enter news text\n'), input('Please enter location\n'))
+            news_mess = self.news
+            news_mess.news_message()
+        elif self.flag == '2':
+            self.advng = Advertising(input('Please enter advertisment text\n'), input('Please enter expire date in the format dd/mm/yy\n'))
+            adv_message = self.advng
+            adv_message.advertising()
+        elif self.flag == '3':
+            self.question = WhoIs(input('Who killed Kennedy? Please enter your assumption\n'))
+            question_mess = self.question
+            question_mess.ask_question()
+        elif self.flag == '4':
+            self.copy_mess = AddInfo('C:/Users/Aleksandra_Vainilovi/PycharmProjects/pythonProject/Module6.0_paste.txt', 'C:/Users/Aleksandra_Vainilovi/PycharmProjects/pythonProject/Module6.0.txt')
+            copy_message = self.copy_mess
+            copy_message.add_message()
+            copy_message.normalize()
+        elif self.flag == '5':
+            self.add_j_son = AddJson('test_mod8.json')
+            add_json = self.add_j_son
+            add_json.parse_json()
+        else:
+            print('Your choice is not correct')
+
+
+make_your_choice = Choice(input('Please enter your choice:\n1 for News message\n2 for Advertisment\n3 for question of a day\n4 for copying existing messages\n5 for existing JSON file data import\n'))
+make_your_choice.choose_message_type()
+
+
+
